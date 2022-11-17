a20e2f4eec35d3e51ac29ce9a5e5cbbe5934947d
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-17 21:39:13 +0800

    fix parcel build

---
+++ dist/index.aa21bb8d.js
@@ -0,0 +1,2 @@
+const inputEl=document.querySelector(".inputEl"),btnEl=Array.from(document.querySelectorAll("button"));inputEl.value="",btnEl.map((btn=>{btn.addEventListener("click",(e=>{switch(e.target.innerText){case"=":try{inputEl.value=eval(inputEl.value)}catch{alert("Error"),inputEl.value=""}break;case"C":inputEl.value="";break;default:inputEl.value+=e.target.innerText}}))}));
+//# sourceMappingURL=index.aa21bb8d.js.map
