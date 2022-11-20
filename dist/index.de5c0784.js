4d1743eb5e254151ccc341f73a0f4bda9d5a9a99
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-20 10:24:29 +0800

    ready for deploy

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -1,9 +1,24 @@
 const inputEl = document.querySelector(".inputEl");
 const btnEl = Array.from(document.querySelectorAll("button"));
+inputEl.value = "";
 btnEl.map((btn)=>{
     btn.addEventListener("click", (e)=>{
-        inputEl.value += e.target.innerText;
-        console.log(e.target.innerText);
+        switch(e.target.innerText){
+            case "=":
+                try {
+                    inputEl.value = eval(inputEl.value);
+                } catch  {
+                    alert("Error");
+                    inputEl.value = "";
+                }
+                break;
+            case "C":
+                inputEl.value = "";
+                break;
+            default:
+                inputEl.value += e.target.innerText;
+                break;
+        }
     });
 });
 
